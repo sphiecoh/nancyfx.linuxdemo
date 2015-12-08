a493da5e84be1d327156cf12afbe59e6eb9071fd
Sphiecoh <sfiso.dlaba@gmail.com>
2015-12-08 12:37:56 +0200

    Simplistic nancy app on linux

---
+++ Nancy.Linux/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Nancy.Linux.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return "Hello world";
+            };
+        }
+    }
+}
---
+++ Nancy.Linux/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using Mono.Unix;
+
+namespace Nancy.Linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri =
+                new Uri("http://localhost:8099");
+            Console.WriteLine($"Your application is running on {uri}");
+            Console.WriteLine("Press any [Enter] to close the host.");
+
+            using (var host = new NancyHost(uri))
+            {
+                host.Start();
+                if(Type.GetType("Mono.Runtime") != null)
+                {
+                    UnixSignal.WaitAny(new[] {
+                        new UnixSignal(Mono.Unix.Native.Signum.SIGINT),
+                        new UnixSignal(Mono.Unix.Native.Signum.SIGTERM),
+                        new UnixSignal(Mono.Unix.Native.Signum.SIGQUIT),
+                        new UnixSignal(Mono.Unix.Native.Signum.SIGHUP)
+                    });
+                }
+               else
+                Console.ReadLine();
+                host.Stop();
+            }
+        }
+    }
+}
